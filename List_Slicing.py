fcb9b06c40bbb3ad5d3306ab40757a8d03073a0f
Paneer Selvam <99405385+paneer24@users.noreply.github.com>
2025-03-31 17:07:01 +0530

    Create List_Slicing.py

---
+++ List_Slicing.py
@@ -0,0 +1,6 @@
+numbers = list(range(1, 11))
+extracted_list = numbers[:5]
+reversed_list = extracted_list[::-1]
+print("Original List:",numbers)
+print("Extracted List:", extracted_list)
+print("Reversed List:", reversed_list)
